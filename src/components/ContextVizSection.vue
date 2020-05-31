5748fe25c43d9ed616489196ee3028e5f206bf0d
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 15:50:19 -0700

    Fix ContextVizSection styles

--- src/components/ContextVizSection.vue
+++ src/components/ContextVizSection.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="h-screen grid grid-cols-1 lg:grid-cols-8 p-16 gap-8">
     <aside class="lg:col-span-2 bg-gray-200">
-      <h1 class="text-4xl p-4">
-        <slot class="title"></slot>
+      <h1 class="text-4xl p-4 text-gray-900">
+        <slot name="title"></slot>
       </h1>
-      <div class="p-4">
+      <div class="text-lg p-4 text-gray-900">
         <slot name="context"></slot>
       </div>
     </aside>
-    <section class="lg:col-span-6 bg-gray-700 p-4 flex">
+    <section class="lg:col-span-6 bg-gray-700 p-4 flex text-white">
       <div class="block w-auto m-auto">
         <slot name="visual"></slot>
       </div>
