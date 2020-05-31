7db5f409bbb4c5bed64aa0a0611de921d0646d55
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 15:50:09 -0700

    Create BigStatement for big quote blocks

---
+++ src/components/templates/BigStatement.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="h-screen p-4 md:p-16 text-white font-display flex">
+    <div class="m-auto text-5xl m-auto max-w-4xl">
+      <slot></slot>
+    </div>
+  </section>
+</template>
