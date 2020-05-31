e4f8f114f8ab15da7afee61e95148ecb3fba8092
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 14:45:44 -0700

    Create basic router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import About from './About.vue'
 
 Vue.config.productionTip = false
 
+const routes = {
+  '/': App,
+  '/about': About,
+}
+
 new Vue({
-  render: h => h(App),
+  // render: h => h(App),
+  data: {
+    currentRoute: window.location.pathname,
+  },
+  computed: {
+    viewComponent() {
+      return routes[this.currentRoute] || routes['/']
+    },
+  },
+  render(h) {
+    return h(this.viewComponent)
+  },
 }).$mount('#app')
