0d4047d09b4155c815d144208f24a1cbffb3c5b3
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 14:45:24 -0700

    Create template for title section

---
+++ src/components/TitleSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="h-screen p-4 md:p-16 text-white">
+    <div class="md:max-w-2xl mx-auto">
+      <div>
+        <!-- Image -->
+      </div>
+      <div>
+        <h1 class="text-4xl text-center">
+          <slot name="title"></slot>
+        </h1>
+        <div class="text-3xl text-center">
+          <slot name="subtitle"></slot>
+        </div>
+        <div class="text-lg">
+          <slot><!-- Default --></slot>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
