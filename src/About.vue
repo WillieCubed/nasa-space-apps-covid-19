60c550cb07d9b7aef4667d3fd1be3baf9e6af38d
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 14:44:29 -0700

    Create temproary about page

---
+++ src/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <header>
+      <div>
+        <a href="/">Back</a>
+      </div>
+    </header>
+    <section id="hero">
+      <h1 class="text-4xl">The Cinquanauts</h1>
+    </section>
+    <!-- TODO: List that staggers about page -->
+  </div>
+</template>
+
+<style>
+
+</style>
