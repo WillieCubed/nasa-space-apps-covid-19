e6e4265bf701399b24a85d5d7b19a9afcf05cba9
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 23:25:03 -0700

    Create three-column layout

---
+++ src/components/templates/ThreeColumn.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="h-screen p-4 md:p-16 text-white grid grid-cols-3 gap-4">
+    <div class="h-full">
+      <slot name="col-1"></slot>
+    </div>
+    <div class="h-full">
+      <slot name="col-2"></slot>
+    </div>
+    <div class="h-full">
+      <slot name="col-3"></slot>
+    </div>
+  </section>  
+</template>
