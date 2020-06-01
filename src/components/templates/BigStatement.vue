34a1583a5fd00df8fea5d560d9b42946c378da19
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 23:25:27 -0700

    Add subtext to big statement

--- src/components/templates/BigStatement.vue
+++ src/components/templates/BigStatement.vue
@@ -1,7 +1,12 @@
 <template>
-  <section class="h-screen p-4 md:p-16 text-white font-display flex">
-    <div class="m-auto text-5xl m-auto max-w-4xl">
-      <slot></slot>
+  <section class="big-statement h-screen p-4 md:p-16 text-white font-display flex">
+    <div class="max-w-4xl m-auto">
+      <div class="m-auto text-5xl max-w-4xl">
+        <slot></slot>
+      </div>
+      <div class="mt-8 text-3xl max-w-4xl">
+        <slot name="subtext"></slot>
+      </div>
     </div>
   </section>
 </template>
