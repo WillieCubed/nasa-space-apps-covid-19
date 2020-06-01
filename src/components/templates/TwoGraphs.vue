f1f00abd8995a69caa4605641a113717ce6b13aa
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-31 23:25:16 -0700

    Create two graph layout

---
+++ src/components/templates/TwoGraphs.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="h-screen p-4 md:p-16 text-white">
+    <div class="flex w-full h-full">
+      <div class="p-4">
+        <slot name="col-1"></slot>
+      </div>
+      <div class="p-4">
+        <slot name="col-2"></slot>
+      </div>
+    </div>
+    <div class="w-full">
+      <slot name="description"></slot>
+    </div>
+  </section>
+</template>
